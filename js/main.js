76baeea3e50def5f6f2247f2c577ae7dde801262
jafnee <jafneejesmee@gmail.com>
2014-04-22 21:56:36 +0100

    Final version for assignment submission

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 define(["model/sounds", "model/images", "model/canvas", "model/character", "model/game", "view/draw", "controller/listener", "controller/action", "controller/keybind", "controller/gameRunner", "controller/localStorageManager"],
     function (Sounds, Images, Canvas, Character, Game, Draw, Listener, Action, Keybind, GameRunner, LSM) {
-        //test
-        //end of test
         LSM.init();
         LSM.load();
         if (!Game.muteMusic && window.requestAnimationFrame !== undefined) {
